ed8f05a7cb4d1d2d5aa7ffc46521d7fe4cdd61a8
Dom Bryan <dombguitar@gmail.com>
2022-01-11 14:42:36 +0000

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Dom Bryan on 11/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
